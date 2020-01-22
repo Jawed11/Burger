d7ca49b1f68986b771d91c446653d274c5cab957
Jawed Ekhlas <jawz07r1@gmail.com>
2020-01-21 19:16:12 -0800

    mysql database connection

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,12 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
-CREATE TABLE burger_db;
-id integer(auto_increment) NOT NULL PRIMARY,
+CREATE TABLE burger_db (
+item_id integer(255) NOT NULL,
 burger_name VARCHAR (255) NOT NULL,
-devoured BOOLEAN NOT NULL,
+devoured BOOLEAN NOT NULL
+);
+
+
 
 
