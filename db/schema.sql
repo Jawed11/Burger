3ef5a9b8b1fc87a28eaa73d300db11d76d271508
Jawed Ekhlas <jawz07r1@gmail.com>
2020-01-21 18:18:14 -0800

    mysql update

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burger_db;
+id integer(auto_increment) NOT NULL PRIMARY,
+burger_name VARCHAR (255) NOT NULL,
+devoured BOOLEAN NOT NULL,
+
+
